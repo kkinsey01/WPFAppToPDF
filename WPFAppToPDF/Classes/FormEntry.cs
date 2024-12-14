1eb0ba44fd97f705b29417a338a2f5e9a7f03ff2
kkinsey01 <kylerkinsey01@gmail.com>
2024-12-14 00:00:47 -0500

    Add project files.

---
+++ WPFAppToPDF/Classes/FormEntry.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAppToPDF.Classes;
+
+public partial class FormEntry : ObservableObject
+{
+    public string Name { get; set; } = "";
+    public string Address { get; set; } = "";
+    public string Email { get; set; } = "";
+}
---
+++ WPFAppToPDF/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFAppToPDF.Classes;
+
+namespace WPFAppToPDF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+[INotifyPropertyChanged]
+public partial class MainWindow : Window
+{
+    [ObservableProperty]
+    private string email;
+
+    [ObservableProperty]
+    private string address;
+
+    [ObservableProperty]
+    private string name;
+
+
+    partial void OnAddressChanged(string value)
+    {
+        formEntry.Address = value;
+    }
+
+    partial void OnEmailChanged(string value)
+    {
+        formEntry.Email = value;
+    }
+
+    partial void OnNameChanged(string value)
+    {
+        formEntry.Name = value;
+    }
+
+    private FormEntry formEntry;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        formEntry = new();
+        Name = "sss";
+        Email = "";
+        Address = "";        
+        DataContext = formEntry;
+    }
+
+    private async void SubmitForm_Click(object sender, RoutedEventArgs e)
+    {
+        
+    }
+}
